434e37c45fd579e73e8c5deaa7e90c777f91127c
weidian <youzhijun@weidian.com>
2019-05-30 19:53:39 +0800

    render函数

---
+++ src/components/api/readme.md
@@ -0,0 +1,13 @@
+#### vue容易忽视的api
+
+##### nextTick
+ 
+- nextTick 是 Vue.js 提供的一个函数，nextTick 函数接收一个回调函数 cb，在下一个 DOM 更新循环之后执行
+
+- v-model 是一个语法糖，可以拆解为 props: value 和 events: input。
+
+- .sync 修饰符 
+
+- $set
+
+- 计算属性的 set
---
+++ src/components/render/render.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="main" class="container" style="color: red">
+    <p v-if="show">内容1</p>
+    <p v-else>内容2</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
